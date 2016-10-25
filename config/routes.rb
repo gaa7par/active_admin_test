c58ec52f54d716327e850a813af3ca5b2770b452
gaa7par <gaa7par@gmail.com>
2016-10-25 10:46:41 +0200

    Generate Articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+
+  get 'articles/show'
+
+  get 'articles/new'
+
+  get 'articles/edit'
+
+  get 'articles/create'
+
+  get 'articles/update'
+
+  get 'articles/destroy'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'home/index'
