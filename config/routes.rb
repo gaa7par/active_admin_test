42ec075d7379d0f9045cb45cc3cb821d2fb68001
gaa7par <gaa7par@gmail.com>
2016-10-25 11:06:31 +0200

    Write views

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-
-  get 'articles/show'
-
-  get 'articles/new'
-
-  get 'articles/edit'
-
-  get 'articles/create'
-
-  get 'articles/update'
-
-  get 'articles/destroy'
+  resources :articles
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
