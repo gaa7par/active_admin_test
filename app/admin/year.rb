b6f4d7a90c9fd30f5fbdd09096cfc7e5c061f0a3
gaa7par <gaa7par@gmail.com>
2016-10-25 14:56:06 +0200

    Activate just_datetime_picker

--- app/admin/year.rb
+++ app/admin/year.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Year do
   permit_params :year
+
+  form do |f|
+    f.inputs do
+      f.input :year, as: :just_datetime_picker
+    end
+
+    f.button
+  end
 end
--- app/models/year.rb
+++ app/models/year.rb
@@ -1,2 +1,4 @@
 class Year < ApplicationRecord
+  just_define_datetime_picker :year, add_to_attr_accessor: true
+  validates :year, presence: true
 end
