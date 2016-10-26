e97fdd34e789272ecee639d0d2b9ce7c8af44f5e
gaa7par <gaa7par@gmail.com>
2016-10-26 15:41:48 +0200

    Customize dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,10 +3,21 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+    columns do
+
+      column do
+        panel "Welcome!" do
+          h1 "Description"
+          div do
+            ul do
+              li "Detailed Description"
+              li "Fancy Charts"
+              li "Rich Documentation"
+              li "Useful links"
+            end
+          end
+        end
       end
     end
   end
