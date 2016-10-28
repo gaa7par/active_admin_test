7cb8586694929a63f7916d5997dd0e9e0fe032cc
gaa7par <gaa7par@gmail.com>
2016-10-28 13:40:59 +0200

    Customize fullcalendar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,5 +19,13 @@
 
 $(document).ready(function() {
   $('#calendar').fullCalendar({
-  })
+    dayClick: function(date, jsEvent, view) {
+      // alert('Clicked on: ' + date.format());
+      // alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+      // alert('Current view: ' + view.name);
+
+      $(this).css('background-color', 'red');
+
+    }
+  });
 });
